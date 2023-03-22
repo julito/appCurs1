afddd041d48d3e46cc6e11da7f02997d9698ef95
julito <jcponce87@gmail.com>
2023-03-22 08:51:08 -0600

    cambios de prueba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,6 @@ export class AppComponent {
     { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
     { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
   ];
-  public labels = ['Familia', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  public labels = ['Familia', 'Amigos', 'Notas', 'Work', 'Travel', 'Reminders'];
   constructor() {}
 }
